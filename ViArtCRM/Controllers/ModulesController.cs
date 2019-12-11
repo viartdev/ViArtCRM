8f2547cb97b54f78dde390ddcbd6bd98acbf1c21
Babichartem <Babichartemson@gmail.com>
2019-12-11 23:40:26 +0300

    Modules added

---
+++ ViArtCRM/Controllers/ModulesController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ViArtCRM.Models;
+
+namespace ViArtCRM.Controllers {
+    public class ModulesController : Controller {
+        public IActionResult Index() {
+            TaskModuleContext context = HttpContext.RequestServices.GetService(typeof(TaskModuleContext)) as TaskModuleContext;
+            return View(context.GetModules());
+        }
+    }
+}
--- ViArtCRM/Controllers/SchedulerController.cs
+++ ViArtCRM/Controllers/SchedulerController.cs
@@ -3,26 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using ViArtCRM.Models;
 
-namespace ViArtCRM.Controllers {
-    public class SchedulerController : Controller {
-
-        public IActionResult Index() {
-            TasksContext context = HttpContext.RequestServices.GetService(typeof(TasksContext)) as TasksContext;
-            return View(context.GetTaskContainer());
-        }
-
-        public IActionResult Create() {
+namespace ViArtCRM.Controllers
+{
+    public class SchedulerController : Controller
+    {
+        public IActionResult Index()
+        {
             return View();
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(Models.Task task) {
-            TasksContext context = HttpContext.RequestServices.GetService(typeof(TasksContext)) as TasksContext;
-            context.InsertTask(task);
-            return RedirectToAction("Index");
         }
     }
 }
