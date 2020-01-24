ce45083716f3136c30462f45494f15977f846ac2
eroticstaff <36076688+eroticstaff@users.noreply.github.com>
2020-01-24 22:36:29 +0300

    Chat groups upgraded

--- ViArtCRM/Models/ChatModel.cs
+++ ViArtCRM/Models/ChatModel.cs
@@ -31,6 +31,12 @@ namespace ViArtCRM.Models
         public MessageObject messageObject { get; set; }
     }
 
+    public class GroupUser
+    {
+        public int GroupUserID { get; set; }
+        public int GroupID { get; set; }
+        public int UserID { get; set; }
+    }
     public class GroupContainer
     { 
         public List<GroupMessage> groupMessageList { get; set; }
@@ -40,32 +46,63 @@ namespace ViArtCRM.Models
         public string ConnectionString { get; set; }
         public GroupContainer GetGroupsContainer()
         {
+            int userID = 1;                                         //userID
             GroupContainer groupContainer = new GroupContainer();
-            groupContainer.groupMessageList = GetGroups();
+            groupContainer.groupMessageList = GetGroup(userID);
             return groupContainer;
         }
         public ChatContext(string connectionString)
         {
             this.ConnectionString = connectionString;
         }
+        public List<GroupUser> getGroupsByUser(int UserID)
+        {
+            List<GroupUser> groups = new List<GroupUser>();
+            SQLSelectQuerySettings sqlSelectQuerySettings = new SQLSelectQuerySettings
+            {
+                ConnectionString = this.ConnectionString,
+                TableName = "ChatGroupUsers",
+                QueryParams = GetQueryParams(UserID),
+            };
+            groups= SQLWrapper.SelectData<GroupUser>(sqlSelectQuerySettings);
+            return groups;
+        }
+        Dictionary<string, string> GetQueryParams(int UserID)
+        {
+            if (UserID == -1)
+                return null;
+            var queryParams = new Dictionary<string, string>();
+            if (UserID != -1)
+                queryParams.Add("UserID", UserID.ToString());
 
-        public List<GroupMessage> GetGroups()
+            return queryParams;
+        }
+        public GroupObject GetGroupByID(int id)
         {
             List<GroupObject> list = new List<GroupObject>();
             SQLSelectQuerySettings sqlSelectQuerySettings = new SQLSelectQuerySettings
             {
                 ConnectionString = this.ConnectionString,
                 TableName = "GroupChat",
-     
+                QueryParams = new Dictionary<string, string>() { { "GroupID", id.ToString() } }
             };
 
             list = SQLWrapper.SelectData<GroupObject>(sqlSelectQuerySettings);
+
+            return list.First(s => s.GroupID == id);
+        }
+        public List<GroupMessage> GetGroup(int UserID)
+        {
+            List<GroupObject> list = new List<GroupObject>();
+            List<GroupUser> groups = getGroupsByUser(UserID);
             List<GroupMessage> groupMessageList = new List<GroupMessage>();
-            foreach (GroupObject item in list)
+
+            foreach (GroupUser item in groups)
             {
+                GroupObject group = GetGroupByID(item.GroupID);
                 GroupMessage newGroup = new GroupMessage();
-                newGroup.groupObject = item;
-                newGroup.messageObject = GetMessageByID(item.LastMessageID);
+                newGroup.groupObject = group;
+                newGroup.messageObject = GetMessageByID(group.LastMessageID);
                 groupMessageList.Add(newGroup);
             }
             return groupMessageList;
